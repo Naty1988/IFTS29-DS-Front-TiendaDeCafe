91585f4849d9811035a9084b32a712a9a73b519b
Estela V. Menéndez <estela.vm@hotmail.com.ar>
2024-10-22 16:14:01 -0300

    Casi se termina nuestromenu.html

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,16 @@ window.addEventListener('scroll', function() {
         }
     });
 });
+
+const sliderImg1 = document.querySelector('.slider-img1');
+const nonVisibleImg = document.querySelector('.slider-img1-nonvisible');
+
+// Mostrar u ocultar la imagen no visible al hacer click en slider-img1
+sliderImg1.addEventListener('click', function() {
+    nonVisibleImg.classList.toggle('show'); // Alterna entre mostrar y ocultar
+});
+
+// Ocultar la imagen no visible al hacer click en la propia imagen no visible
+nonVisibleImg.addEventListener('click', function() {
+    nonVisibleImg.classList.remove('show'); // Se oculta al hacer click sobre la imagen grande
+});
